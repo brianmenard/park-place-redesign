f3eba3f33b2ac17c77da0a9cbc54de40f5d0b1d7
Brian <useyourbrian@gmail.com>
2016-07-10 13:10:58 -0400

    added more states, created base templates for each state

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,8 +6,28 @@ angular
     .module('app', ['ui.router'])
     .config(function($stateProvider){
         $stateProvider
-            .state('test', {
-                url: '/test',
-                templateUrl: 'templates/test.html'
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/index.html'
+            })
+
+            .state('about', {
+              url: '/about',
+              templateUrl: 'templates/about.html'
+            })
+
+            .state('neighborhood',{
+              url: '/neighborhood',
+              templateUrl:'templates/neighborhood.html'
+            })
+
+            .state('lifestyle', {
+              url: '/lifestyle',
+              templateUrl: 'templates/lifestyle.html'
+            })
+
+            .state('listings', {
+              url: 'listings',
+              templateUrl: 'templates/listings.html'
             })
     });
