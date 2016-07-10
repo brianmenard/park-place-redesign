d590e2515dbca67f6bb6541c6d3737b481dc8b7a
Brian <useyourbrian@gmail.com>
2016-07-10 19:22:50 -0400

    added ng-model input search bar, added search functions in listingCtrl, added price and description fields to listing data

--- app/assets/javascripts/controllers/listingctrl.js
+++ app/assets/javascripts/controllers/listingctrl.js
@@ -1,9 +1,24 @@
-function listingCtrl($scope){
+function listingCtrl($scope, $filter){
   $scope.listings = [
-    {address: 'Unit 3908'},
-    {address: 'Unit 5243'},
-    {address: 'Unit 3407'}
+    {address: 'Unit 3908',
+      price: '$1700/mo',
+      description: 'Lovely one bedroom with large windows'},
+    {address: 'Unit 5243',
+      price: '$2450/mo',
+      description: 'Two bedroom on the 52nd floor, pet friendly'},
+    {address: 'Unit 3407',
+      price: '$1650/mo',
+      description: 'One bedroom with newly renovated kitchen'}
   ];
+
+  $scope.searchInput = '';
+
+  $scope.refilter = function(){
+    $scope.filteredList = $filter('filter')(this.listings, this.searchInput);
+  };
+
+  $scope.refilter();
+
 }
 
 angular
