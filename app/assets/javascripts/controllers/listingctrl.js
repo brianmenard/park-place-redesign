797ed8f869ecb17b530f7534517d457a4216ec71
Brian <useyourbrian@gmail.com>
2016-07-10 20:11:08 -0400

    added bedroom and bathroom filters on listings page

--- app/assets/javascripts/controllers/listingctrl.js
+++ app/assets/javascripts/controllers/listingctrl.js
@@ -2,19 +2,29 @@ function listingCtrl($scope, $filter){
   $scope.listings = [
     {address: 'Unit 3908',
       price: '$1700/mo',
+      bedrooms: '1',
+      bathrooms: '1',
       description: 'Lovely one bedroom with large windows'},
     {address: 'Unit 5243',
       price: '$2450/mo',
+      bedrooms: '2',
+      bathrooms: '1',
       description: 'Two bedroom on the 52nd floor, pet friendly'},
     {address: 'Unit 3407',
       price: '$1650/mo',
+      bedrooms: '1',
+      bathrooms: '2',
       description: 'One bedroom with newly renovated kitchen'}
   ];
 
   $scope.searchInput = '';
 
+  $scope.bedroomFilter = '';
+
+  $scope.bathroomFilter = '';
+
   $scope.refilter = function(){
-    $scope.filteredList = $filter('filter')(this.listings, this.searchInput);
+    $scope.listings = $filter('filter')($scope.listings, this.searchInput);
   };
 
   $scope.refilter();
